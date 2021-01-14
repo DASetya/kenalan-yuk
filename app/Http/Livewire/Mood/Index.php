48fbf45dfc188bcb5aa2e972bbeb8c55a1fc3565
Anwarul Fattach <emosekolah@gmail.com>
2021-01-14 19:05:04 +0700

    new set up chart js

--- app/Http/Livewire/Mood/Index.php
+++ app/Http/Livewire/Mood/Index.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    public $data;
+    public $labels;
+
+    public function mount()
+    {
+        $this->data = [12, 19, 3, 5];
+        $this->labels = ["20 Mei", "20 Mei", "20 Mei", "20 Mei"];
+    }
+
     public function render()
     {
         return view('livewire.mood.index');
--- resources/views/livewire/mood/index.blade.php
+++ resources/views/livewire/mood/index.blade.php
@@ -1,28 +1,18 @@
 @section('scripts')
 <script>
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
+    let ctx = document.getElementById('myChart');
+    let myChart = new Chart(ctx, {
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: {!! json_encode($labels) !!},
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Tingkat Kebahagiaan',
+                data: {{ json_encode($data) }},
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -43,7 +33,7 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold leading-tight">
-            Mood
+            Mood {{ json_encode($labels) }}
             <a href="#" class="text-2xl"><i class="far fa-question-circle"></i></a>
         </h2>
     </x-slot>
