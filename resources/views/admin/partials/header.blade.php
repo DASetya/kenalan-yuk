891f903fdcf6ab42ba7f29f95e99f6c24d83a515
Anwarul Fattach <emosekolah@gmail.com>
2021-01-15 01:50:03 +0700

    fix gmap

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-<title>Blank Page &mdash; Stisla</title>
+<title>Kenalan Yuk</title>
 
 @include('admin.partials.styles')
