3e0ecaa4fed82c4b82c173a92eea6ac7cef4b7d7
Anwarul Fattach <emosekolah@gmail.com>
2021-01-18 02:15:58 +0700

    halaman login done, tinggal pasang font trus upload

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,52 +1,55 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0">
+        <div class="flex text-center w-full sm:max-w-md">
+            <a href="{{ route('index') }}" class="text-2xl text-primary my-auto ml-3"><i class="fas fa-chevron-left"></i></a>
+            <h1 class="text-4xl font-bold mx-auto">
+                Masuk
+            </h1>
+        </div>
+    
+        <div class="w-full sm:max-w-md px-6 py-3 sm:rounded-lg">
+            <x-jet-validation-errors class="mb-4" />
 
-        <x-jet-validation-errors class="mb-4" />
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+    
+                <div>
+                    <x-jet-input id="email" class="block mt-1 w-full rounded-full" type="email" name="email" :value="old('email')" required autofocus placeholder="Masukkan Email" />
+                </div>
+    
+                <div class="mt-4">
+                    <x-jet-input id="password" class="block mt-1 w-full rounded-full" type="password" name="password" required autocomplete="current-password" placeholder="Masukkan Password" />
+                </div>
+    
+                {{-- <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div> --}}
+    
+                <div class="flex items-center justify-center mt-4">
+                    {{-- @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif --}}
+    
+                    {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        Belum punya akun ?, Daftar
+                    </a> --}}
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                {{-- @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif --}}
-
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    Belum punya akun ?, Daftar
-                </a>
-
-                <x-jet-button class="button button-light ml-4">
-                    {{ __('Login') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+                    <button class="button-plain button-primary text-xl font-bold shadow-lg w-1/2">
+                        Masuk
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 <x-guest-layout>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0">
-        <div class="text-center w-full flex">
+        <div class="flex text-center w-full sm:max-w-md">
             <a href="{{ route('index') }}" class="text-2xl text-primary my-auto ml-3"><i class="fas fa-chevron-left"></i></a>
             <h1 class="text-4xl font-bold mx-auto">
                 Daftar
             </h1>
         </div>
     
-        <div class="w-full sm:max-w-md px-6 py-3 overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md px-6 py-3 sm:rounded-lg">
             <x-jet-validation-errors class="mb-4" />
 
             <form method="POST" action="{{ route('register') }}">
@@ -30,7 +30,7 @@
                 </div>
 
                 <div class="flex items-center justify-center mt-5">
-                    <button class="button-plain button-primary ml-4 text-xl font-bold shadow-md">
+                    <button class="button-plain button-primary text-xl font-bold shadow-lg">
                         Daftar Sekarang!
                     </button>
                 </div>
