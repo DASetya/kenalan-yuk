d5282d331979914d9dd2ea429f54d4c81e40a056
Anwarul Fattach <emosekolah@gmail.com>
2021-01-18 02:02:57 +0700

    halaman daftar done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,40 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0">
+        <div class="text-center w-full flex">
+            <a href="{{ route('index') }}" class="text-2xl text-primary my-auto ml-3"><i class="fas fa-chevron-left"></i></a>
+            <h1 class="text-4xl font-bold mx-auto">
+                Daftar
+            </h1>
+        </div>
+    
+        <div class="w-full sm:max-w-md px-6 py-3 overflow-hidden sm:rounded-lg">
+            <x-jet-validation-errors class="mb-4" />
 
-        <x-jet-validation-errors class="mb-4" />
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+                <div>
+                    <x-jet-input id="name" class="block mt-1 w-full rounded-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Masukkan Username"/>
+                </div>
 
-            <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+                <div class="mt-4">
+                    <x-jet-input id="email" class="block mt-1 w-full rounded-full" type="email" name="email" :value="old('email')" required placeholder="Masukkan email yang aktif"/>
+                </div>
 
-            <div class="mt-4">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
+                <div class="mt-4">
+                    <x-jet-input id="password" class="block mt-1 w-full rounded-full" type="password" name="password" required autocomplete="new-password" placeholder="Masukkan password"/>
+                </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                <div class="mt-4">
+                    <x-jet-input id="password_confirmation" class="block mt-1 w-full rounded-full" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Ulangi password Anda"/>
+                </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    Sudah punya akun ?, Masuk
-                </a>
-
-                <x-jet-button class="button button-light ml-4">
-                    Daftar
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+                <div class="flex items-center justify-center mt-5">
+                    <button class="button-plain button-primary ml-4 text-xl font-bold shadow-md">
+                        Daftar Sekarang!
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-guest-layout>
